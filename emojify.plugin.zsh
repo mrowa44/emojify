0a4d571d1b7a28ffdbf0eb4b9b17c39ddd5566df
Joe Block <jpb@unixorn.net>
2015-07-25 13:56:23 -0700

    Add a ZSH plugin support file
    Added a zsh plugin support file so that the repo can be directly
    installed into their environment by ZSH framework users.
    
    Added instructions for loading the repo as a plugin to README.md

---
+++ emojify.plugin.zsh
@@ -0,0 +1,7 @@
+# Licensed under the MIT license
+#
+# See https://github.com/mrowa44/emojify/blob/master/LICENSE.md
+
+# Add ourself to the user's path
+PLUGIN_BIN="$(dirname $0)"
+export PATH=${PATH}:${PLUGIN_BIN}
